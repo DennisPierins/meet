ab09898f586efcc309b7a364beb92cdef502dfaf
DennisPierins <dennispierins@gmail.com>
2021-01-17 14:53:18 +0100

    Added service-worker.js

--- src/index.js
+++ src/index.js
@@ -12,9 +12,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 serviceWorker.register();
 
 atatus.config('f86fc2e5e5cb4dadb06d7486c677ef51').install();
