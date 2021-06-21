fa3859757a56516f8780680eb38cc461ce69ae37
DennisPierins <dennispierins@gmail.com>
2021-06-21 15:58:46 +0200

    Added a background colour and made the piechart not overlap

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,23 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import { ResponsiveContainer, PieChart, Pie, Cell } from 'recharts';
+import React, { useEffect, useState } from "react";
+import { ResponsiveContainer, PieChart, Pie, Cell } from "recharts";
 
 const EventGenre = ({ events }) => {
   const [data, setData] = useState([]);
-  useEffect(() => { setData(() => getData()); }, [events]);
+  useEffect(() => {
+    setData(() => getData());
+  }, [events]);
 
   const getData = (genre) => {
-    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+    const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS"];
     const data = genres.map((genre) => {
-      const value = events.filter(({ summary }) => summary.split(' ').includes(genre)).length;
+      const value = events.filter(({ summary }) =>
+        summary.split(" ").includes(genre)
+      ).length;
       return { name: genre, value };
     });
     return data;
   };
 
-  const colors = ['#8886d5', '#85a7ea', '#8fd1e0', '#84c99e', '#a5dc72'];
+  const colors = ["#8886d5", "#85a7ea", "#8fd1e0", "#84c99e", "#a5dc72"];
 
   return (
-    <ResponsiveContainer height={400} >
+    <ResponsiveContainer height={400}>
       <PieChart height={400}>
         <Pie
           data={data}
@@ -27,12 +31,19 @@ const EventGenre = ({ events }) => {
           outerRadius={80}
           fill="#8884d8"
           dataKey="value"
-          label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`} >
-          {data.map((entry, index) => (<Cell key={`cell-${index}`} fill={colors[index]} />))}
+          label={({ name, percent }) =>
+            `${percent > 0 ? name : ""} ${
+              percent > 0 ? (percent * 100).toFixed(0) + "%" : ""
+            }`
+          }
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index]} />
+          ))}
         </Pie>
       </PieChart>
     </ResponsiveContainer>
   );
-}
+};
 
 export default EventGenre;
